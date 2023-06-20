3781a6afbe9ac72bcaa88a95cffb9dceddcc2214
Santiago <santiagovalencialeon@gmail.com>
2023-06-20 10:29:49 -0500

    sldakjfbkldasjfb

---
+++ Proyecto-8/proyecto08Ejercicio07SantiagoValenciaLeon.JuanPabloSanchez.js
@@ -0,0 +1,11 @@
+// 7. Escribir un programa que pida como datos de entrada el nombre, apellido y año de nacimiento y luego los imprima juntos.
+
+// let nombre = prompt("Ingrese su nombre");
+// let apellido = prompt("Ingrese su apellido");
+// let añoNacimiento = prompt("Ingrese su año de nacimiento");
+
+let nombre = "Tom";
+let apellido = "Davis";
+let añoNacimiento = "1972";
+
+console.log(nombre + " " + apellido + " " + añoNacimiento);
