d19736a980bf30789c616e658fe510fc2ca9f703
Juan Pablo <juan.sanchez8@utp.edu.co>
2023-06-20 09:49:32 -0500

    añadido puntos: 2 y 4

---
+++ Proyecto-8/proyecto08Ejercicio04SantiagoValencia.JuanPabloSanchez.js
@@ -0,0 +1,17 @@
+/*EJERCICIO 04 - PROYECTO 08
+2. Escribir un programa que convierta de Km/h a mph (millas por hora), teniendo en cuenta que 1Km =
+0.621371 millas
+-----------------------------------------------------------------
+Fecha de publicación: 20-06-2023
+Hora:
+Versión: 1.0
+Autores: Ing(c) Santiago Valencia, Ing(c) Juan Pablo Sánchez
+Lenguaje: JavaScript
+Versión del lenguaje: 6.0
+Presentado a: Doctor Ricardo Moreno Laverde
+-----------------------------------------------------------------
+Universidad Tecnológica de Pereira
+Programa de Ingeniería de Sistemas y Computación
+-----------------------------------------------------------------
+Descripción: Este programa
+*/
