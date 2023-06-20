1bf7203f0a69367912465d04a616ecb20afd0036
stivcs <stivencsoto@gmail.com>
2023-06-20 09:48:11 -0500

    añadido puntos: 3 y 5

--- Proyecto-8/proyecto08Ejercicio01SantiagoValenciaLeon.JuanPabloSanchez.js
+++ Proyecto-8/proyecto08Ejercicio01SantiagoValenciaLeon.JuanPabloSanchez.js
@@ -1,11 +1,13 @@
-// 1. Escribir un programa que convierta de grados centígrados a grados fahrenheit.
-const prompt=require("prompt-sync")({sigint:true});
 
-let grados = prompt("Ingrese grados: ");
+// 1. Escribir un programa que convierta de grados centígrados a grados fahrenheit.
+
+
+// let grados = prompt("Ingrese grados: ");
+let grados = 90;
 
 function centigradosAFarenheit(centigrados) {
-    let farenheit = (9/5 * centigrados) + 32.0;
-    return farenheit;
+  let farenheit = (9 / 5) * centigrados + 32.0;
+  return farenheit;
 }
 
 console.log(centigradosAFarenheit(grados));
