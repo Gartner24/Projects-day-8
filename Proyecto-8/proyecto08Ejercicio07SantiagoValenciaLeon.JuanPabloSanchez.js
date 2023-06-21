1f06b794ccd388fa8cc4b280cc805cc24fe1399a
Santiago <santiagovalencialeon@gmail.com>
2023-06-20 22:18:12 -0500

    Documentacion para el proyecto 8

--- Proyecto-8/proyecto08Ejercicio07SantiagoValenciaLeon.JuanPabloSanchez.js
+++ Proyecto-8/proyecto08Ejercicio07SantiagoValenciaLeon.JuanPabloSanchez.js
@@ -1,12 +1,27 @@
-// const prompt = require("prompt-sync")({sigint: true});
-// 7. Escribir un programa que pida como datos de entrada el nombre, apellido y año de nacimiento y luego los imprima juntos.
+/*EJERCICIO 05 - PROYECTO 08
+7. Escribir un programa que pida como datos de entrada el nombre, apellido y año de nacimiento y luego los imprima juntos.
 
-// let nombre = prompt("Ingrese su nombre");
-// let apellido = prompt("Ingrese su apellido");
-// let añoNacimiento = prompt("Ingrese su año de nacimiento");
+-----------------------------------------------------------------
+Fecha de publicación: 20-06-2023
+Hora: 10:22 a.m
+Versión: 1.0
+Autores: Ing(c) Santiago Valencia, Ing(c) Juan Pablo Sánchez
+Lenguaje: JavaScript
+Versión del lenguaje: 6.0
+Presentado a: Doctor Ricardo Moreno Laverde
+-----------------------------------------------------------------
+Universidad Tecnológica de Pereira
+Programa de Ingeniería de Sistemas y Computación
+-----------------------------------------------------------------
+Descripción: Este programa pide como datos de entrada el nombre, apellido y año de nacimiento y luego los imprime juntos.
+*/
 
-let nombre = "Tom";
-let apellido = "Davis";
-let añoNacimiento = "1972";
+const prompt = require("prompt-sync")({sigint: true});
 
+// Variables que almacena el nombre, el apellido y el año de nacimiento
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let añoNacimiento = prompt("Ingrese su año de nacimiento");
+
+// Función que imprime el nombre, el apellido y el año de nacimiento
 console.log(nombre + " " + apellido + " " + añoNacimiento);
